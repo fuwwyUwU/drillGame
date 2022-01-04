b3ff7722af79ccd8cd1a60c64b3af454a547bb2e
fuwwyUwU <vsjoqvist@gmail.com>
2022-01-04 23:30:25 +0100

    started working on terrain generation

---
+++ Tiles/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace drillGame
+{
+    public class Tile
+    {
+        public Vector2 position;
+        public readonly Texture2D sprite;
+        public const int size = 16;
+
+        public Tile(Vector2 _position, Texture2D _sprite)
+        {
+            position = _position;
+            sprite = _sprite;
+        }
+    }
+}
