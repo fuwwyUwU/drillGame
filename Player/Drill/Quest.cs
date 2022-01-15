9b20cdbfe56e02fb96ad0b480a12a1d263e2dd16
EtichBruh <avfetich@gmail.com>
2022-01-15 22:10:03 +0300

    quest

---
+++ Player/Drill/Quest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nekoT
+{
+    public static class Quest
+    {
+        public static string QuestString = "Not Null";
+        private static Vector2 vector = Vector2.Zero;
+        public static void DrawQuest(SpriteBatch _spritebatch, SpriteFont font)
+        {
+
+            _spritebatch.DrawString(font, QuestString, new(QuestString.Length, 0), Color.MonoGameOrange);
+        }
+    }
+}
